b71f4a3b56950529b8ce0253fd1a488ea8a0cfc6
darkangel01111991 <darkangel01111991@gmail.com>
2020-10-27 07:34:42 +0100

    add empty test list for message.test

---
+++ server/topics/message/message.test.js
@@ -0,0 +1,21 @@
+import { setUp } from 'server/helpers/tester.base';
+import { InitDBService } from 'initdb/initdb.service';
+import * as Testers from 'server/tests/testers';
+
+describe('# Message Tests', async () => {
+  setUp(async () => InitDBService.truncateTables());
+
+  it('should publish a new message', () => {});
+  it('shouldnt publish a new message if not logged', () => {});
+  it('should update an existing message only if mine', () => {});
+  it('shouldnt update an existing message if not mine', () => {});
+  it('should get a public message even if not mine', () => {});
+  it('shouldnt get a private message if not mine', () => {});
+  it('should get a private message if mine', () => {});
+  it('should count a new view if message get by another user', () => {});
+  it('should comment a public message', () => {});
+  it('shouldnt comment a private message', () => {});
+  it('should like a public message', () => {});
+  it('shouldnt be able to like a private message', () => {});
+  it('should unlike a public message', () => {});
+});
---
+++ server/topics/message/message.tester.js
@@ -0,0 +1,17 @@
+import httpStatus from 'http-status';
+import request from 'supertest';
+import app from 'server/index'; // eslint-disable-line no-unused-vars
+import expect from 'server/helpers/test.framework';
+
+
+export const publishMessage = async (user, message, { status = httpStatus.OK } = {}) => {
+  const res = await request(app)
+    .post('/api/messages')
+    .set('cookie', user.cookie)
+    .send(message)
+    .expect(status);
+  const messageRes = res.body.message;
+  expect(messageRes).to.containSubset(message);
+  message.id = messageRes.id; //eslint-disable-line no-param-reassign
+  return messageRes;
+};
