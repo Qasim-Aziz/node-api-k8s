df17eea0b227069069bca07620ac673252e229ae
darkangel01111991 <darkangel01111991@gmail.com>
2021-01-25 23:00:13 +0100

    fix issue with init data

--- src/server/topics/init-data/init-data.controller.ts
+++ src/server/topics/init-data/init-data.controller.ts
@@ -2,7 +2,6 @@ import {
   validation, Auth, Env, BackError,
 } from 'src/server/helpers';
 import { InitDBService } from 'src/initdb/initdb.service';
-import { runMigrations } from 'src/migrations/lib/migrate-runner';
 import { populateInitData } from 'src/init-data/init-data.utils';
 
 export class InitDataController {
@@ -11,7 +10,6 @@ export class InitDataController {
   static async initData(req) {
     if (Env.isProd) throw new BackError('Cannot perform init in prod env !');
     await InitDBService.initDb();
-    await runMigrations();
     await populateInitData();
     return { status: 'ok' };
   }
